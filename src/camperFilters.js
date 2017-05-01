0928f015a1aafb03b3e7089de0cda195b9a82ea1
Anthony Cook <ddragonmaster49@yahoo.com>
2017-05-01 00:20:38 -0400

    Add constants used for filtering

---
+++ src/camperFilters.js
@@ -0,0 +1,4 @@
+export const SHOW_ALL_TIME_ASCENDING = "show_all_time_ascending";
+export const SHOW_ALL_TIME_DESCENDING = "show_all_time_descending";
+export const SHOW_RECENT_ASCENDING = "show_recent_ascending";
+export const SHOW_RECENT_DESCENDING = "show_recent_descending";
